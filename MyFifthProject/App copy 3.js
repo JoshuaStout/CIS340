de7abbaf35552b169540caa1597fb68c2373f0bc
JoshuaStout <63564560+JoshuaStout@users.noreply.github.com>
2020-05-04 09:01:51 +0000

    My Fifth Project - Styles and layout

---
+++ MyFifthProject/App copy 3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'red'}}/>
+      <View style={{flex: 2, backgroundColor: 'yellow'}}/>
+      <View style={{flex: 3, backgroundColor: 'blue'}}></View>
+      <View style={{flex: 4, backgroundColor: 'purple'}}></View>
+
+    </View>
+  );
+}
+
